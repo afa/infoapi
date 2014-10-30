a9adbfafad56a729635946f1dab4dc10d37ad7da
Artem Shpakov <artshpakov@yandex.ru>
2014-10-30 15:15:06 +0400

    Prod cap recipe

---
+++ config/deploy/production2.rb
@@ -0,0 +1,16 @@
+set :host, "178.63.13.210"
+
+set :stage,     :production2
+set :user,      "rbdev"
+set :deploy_to, "/home/#{fetch :user}/#{fetch :application}"
+set :branch,    "master"
+
+set :rvm_type, :user
+set :rvm_ruby_version, '2.1.3'
+
+server fetch(:host), user: fetch(:user), roles: %w{web app db}, primary: true
+  
+set :ssh_options, {
+  forward_agent: true,
+  auth_methods: %w(publickey)
+ }
