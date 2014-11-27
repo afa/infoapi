cbb55cd9230ba75141ff927300c54f47b3fb7a11
afa <afa@afanote>
2014-11-27 12:57:42 +0300

    fix trw-538 velosiped

--- db/migrate/014_add_fields_to_refs.rb
+++ db/migrate/014_add_fields_to_refs.rb
@@ -3,7 +3,6 @@ Sequel.migration do
     alter_table :refs do
       add_column :json, String, text: true
       add_column :rule_id, Integer, null: false
-      # add_column :sitemap_session_id, Integer, null: false
     end
     add_index :refs, :rule_id
     add_index :refs, :sitemap_session_id
