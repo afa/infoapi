926d7108888759fe1e30ca7c0c15349174f04aba
afa <afa@afanote>
2014-11-28 18:12:34 +0300

    fix

--- models/simple_api/rules.rb
+++ models/simple_api/rules.rb
@@ -1,7 +1,7 @@
 require 'pp'
 module SimpleApi
-# require 'simple_api'
-# require 'simple_api/rule'
+  # require 'simple_api'
+  # require 'simple_api/rule'
   class Rules
     class << self
       def init(config)
@@ -19,48 +19,49 @@ module SimpleApi
       def prepare_params(params)
         prm = OpenStruct.new(JSON.load(params))
         unless  prm.data.nil? && prm.filters.nil?
-        if prm.data.nil?
-          prm.data = prm.filters
-        end
-        if prm.filters.nil?
-          prm.filters = prm.data
-        end
-        if prm.filters["path"] && prm.filters["catalog"].nil?
-          prm.filters["catalog"] = prm.filters["path"]
-        end
-        if prm.filters["catalog"] && prm.filters["path"].nil?
-          prm.filters["path"] = prm.filters["catalog"]
+          if prm.data.nil?
+            prm.data = prm.filters
+          end
+          if prm.filters.nil?
+            prm.filters = prm.data
+          end
+          if prm.filters["path"] && prm.filters["catalog"].nil?
+            prm.filters["catalog"] = prm.filters["path"]
+          end
+          if prm.filters["catalog"] && prm.filters["path"].nil?
+            prm.filters["path"] = prm.filters["catalog"]
+          end
         end
         prm
       end
 
-      def process(params, sphere, logger)
-        found = Rule.find_rule(sphere, params, @rules)
-        logger.info "for sphere #{sphere} with params #{params.inspect} selected rule #{found.is_a?(Array) ? found.first.try(:id) : found.try(:id)} #{found.is_a?(Array) ? found.first.try(:name) : found.try(:name)}."
-        content = found.kind_of?(Array) ? found.try(:first).try(:content) : found.try(:content)
-      end
-
-      def generate(sitemap = nil)
-        SimpleApi::Rule.where(param: ['rating', 'rating-annotation']).where('traversal_order is not null').order(:position).all.map{|item| SimpleApi::Rule.from_param(item.sphere, item.param)[item.id] }.select{|rul| t = JSON.load(rul.traversal_order) rescue []; t.is_a?(::Array) && t.present? }.each do |rule|
-          next if rule.traversal_order.blank?
-          rule.generate(sitemap)
+        def process(params, sphere, logger)
+          found = Rule.find_rule(sphere, params, @rules)
+          logger.info "for sphere #{sphere} with params #{params.inspect} selected rule #{found.is_a?(Array) ? found.first.try(:id) : found.try(:id)} #{found.is_a?(Array) ? found.first.try(:name) : found.try(:name)}."
+          content = found.kind_of?(Array) ? found.try(:first).try(:content) : found.try(:content)
         end
-        rework(sitemap)
-      end
 
-      def rework(sitemap)
-        DB[:refs].where(sitemap_session_id: sitemap).order(:id).each do |ref|
-          duble = DB[:refs].where{ Sequel.&( ( id < ref[:id]), { :url => ref[:url] }) }.order(:id).first
-          param = JSON.load(ref[:json])
-          rule = SimpleApi::Rule[param["rule"]]
-          Sentimeta.env   = CONFIG["fapi_stage"] # :production is default
-          Sentimeta.lang  = rule.lang.to_sym
-          Sentimeta.sphere = rule.sphere
-          path = param.delete("path").to_s.split(',')
-          empty = (Sentimeta::Client.fetch :objects, {"is_empty" => true}.merge("criteria" => [param.delete('criteria')], "filters" => param.delete_if{|k, v| k == 'rule' }.merge(path.empty? ? {} : {"catalog" => path + (['']*3).drop(path.size)})) rescue {})["is_empty"]
-          DB[:refs].where(:id => ref[:id]).update(:is_empty => empty, :duplicate_id => duble.try(:[], :id))
+        def generate(sitemap = nil)
+          SimpleApi::Rule.where(param: ['rating', 'rating-annotation']).where('traversal_order is not null').order(:position).all.map{|item| SimpleApi::Rule.from_param(item.sphere, item.param)[item.id] }.select{|rul| t = JSON.load(rul.traversal_order) rescue []; t.is_a?(::Array) && t.present? }.each do |rule|
+            next if rule.traversal_order.blank?
+            rule.generate(sitemap)
+          end
+          rework(sitemap)
+        end
+
+        def rework(sitemap)
+          DB[:refs].where(sitemap_session_id: sitemap).order(:id).each do |ref|
+            duble = DB[:refs].where{ Sequel.&( ( id < ref[:id]), { :url => ref[:url] }) }.order(:id).first
+            param = JSON.load(ref[:json])
+            rule = SimpleApi::Rule[param["rule"]]
+            Sentimeta.env   = CONFIG["fapi_stage"] # :production is default
+            Sentimeta.lang  = rule.lang.to_sym
+            Sentimeta.sphere = rule.sphere
+            path = param.delete("path").to_s.split(',')
+            empty = (Sentimeta::Client.fetch :objects, {"is_empty" => true}.merge("criteria" => [param.delete('criteria')], "filters" => param.delete_if{|k, v| k == 'rule' }.merge(path.empty? ? {} : {"catalog" => path + (['']*3).drop(path.size)})) rescue {})["is_empty"]
+            DB[:refs].where(:id => ref[:id]).update(:is_empty => empty, :duplicate_id => duble.try(:[], :id))
+          end
         end
       end
     end
   end
-end
