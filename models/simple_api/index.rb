255218e68b72265dac2b18f158a526bff3e0046d
afa <afa@afanote>
2015-01-15 20:46:01 +0300

    fix

--- models/simple_api/index.rb
+++ models/simple_api/index.rb
@@ -155,7 +155,7 @@ module SimpleApi
           rsp['total'] = nxt.size
         end
         rsp['ratings'] = index_links(bcr, curr, route, 'rating')
-        p rsp['ratings']
+        p 'rat', rsp['ratings']
         # if rsp['ratings'].present?
         #   rsp.delete('next')
         #   rsp.delete('total')
