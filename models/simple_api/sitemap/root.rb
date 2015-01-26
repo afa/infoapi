b0461d5559361347e0f6c649e5ea661e46c4f7dd
afa <afa@afanote>
2015-01-26 03:42:18 +0300

    fix

--- models/simple_api/sitemap/root.rb
+++ models/simple_api/sitemap/root.rb
@@ -11,6 +11,14 @@ module SimpleApi
         ]
       end
 
+      def clean_index
+        SimpleApi::Sitemap::Index.where(root_id: pk).all.each do |index|
+          index.objects.each{|o| o.delete }
+          index.references.each{|r| r.delete }
+        end
+        SimpleApi::Sitemap::Index.where(root_id: pk).delete
+        delete
+      end
     end
   end
 end
