3e882066881d22cbd3aa4b1e1997f171e13cf29c
afa <afa@afanote>
2015-02-03 01:20:36 +0300

    fc

--- models/simple_api/sitemap.rb
+++ models/simple_api/sitemap.rb
@@ -36,16 +36,16 @@ module SimpleApi
     #   end
     # end
 
-    #   def preload_criteria
-    #     Sentimeta.env = CONFIG["fapi_stage"] || :production
-    #     sp_list = ((Sentimeta::Client.spheres rescue []) || []).map{|s| s["name"] }
-    #     DB[:criteria].delete
-    #     (SimpleApi::Rule.all.map(&:sphere).uniq & sp_list).each do |sphere|
-    #       Sentimeta.lang = :en
-    #       Sentimeta.sphere = sphere
-    #       DB[:criteria].multi_insert(((Sentimeta::Client.criteria(:subcriteria => true) rescue []) || []).map{|h| h.has_key?('subcriteria') ? h['subcriteria'] : [h] }.flatten.map{|h| {label: h["label"], name: h["name"], sphere: sphere} })
-    #     end
-    #   end
+      def preload_criteria
+        Sentimeta.env = CONFIG["fapi_stage"] || :production
+        sp_list = ((Sentimeta::Client.spheres rescue []) || []).map{|s| s["name"] }
+        DB[:criteria].delete
+        (SimpleApi::Rule.all.map(&:sphere).uniq & sp_list).each do |sphere|
+          Sentimeta.lang = :en
+          Sentimeta.sphere = sphere
+          DB[:criteria].multi_insert(((Sentimeta::Client.criteria(:subcriteria => true) rescue []) || []).map{|h| h.has_key?('subcriteria') ? h['subcriteria'] : [h] }.flatten.map{|h| {label: h["label"], name: h["name"], sphere: sphere} })
+        end
+      end
 
     #   def rework_links(scope)
     #     # by rules
