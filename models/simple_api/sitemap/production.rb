175e3114a74a732e781ab6667ab206452046da5a
afa <afa@afanote>
2015-04-15 01:23:15 +0300

    fix

--- models/simple_api/sitemap/production.rb
+++ models/simple_api/sitemap/production.rb
@@ -214,7 +214,7 @@ module SimpleApi
         p root
         p root.try(:sphere)
         SimpleApi::Sitemap::Vocabula::VOCABULAS[root.sphere].each do |attr|
-          SimpleApi::Rule.where(sphere: root.sphere, param: %w(group rating rating-annotation)).map(&:lang).each do |lang|
+          SimpleApi::Rule.where(sphere: root.sphere, param: %w(group rating rating-annotation)).all.map(&:lang).uniq.each do |lang|
             SimpleApi::Sitemap::Vocabula.take(root.sphere, lang, attr) unless SimpleApi::Sitemap::Vocabula.fresh?(root.sphere, lang, attr)
           end
         end
