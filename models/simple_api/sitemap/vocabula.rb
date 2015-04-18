3fc7a865a50594518d00acfc3ed2ea7c85b047d1
afa <afa@afanote>
2015-04-18 04:36:28 +0300

    fix

--- models/simple_api/sitemap/vocabula.rb
+++ models/simple_api/sitemap/vocabula.rb
@@ -9,11 +9,10 @@ class SimpleApi::Sitemap::Vocabula < Sequel::Model
 
   def self.fresh?(sphere, lang, attribute)
     (
-      where(lang: lang.to_s, sphere: sphere, kind: attribute).empty?
-      || (
+      where(lang: lang.to_s, sphere: sphere, kind: attribute).empty? ||
+      (
         (
-          where(lang: lang.to_s, sphere: sphere, kind: attribute).reverse_order(:id).first.try(:created_at)
-          || Time.new(0)
+          where(lang: lang.to_s, sphere: sphere, kind: attribute).reverse_order(:id).first.try(:created_at) || Time.new(0)
         ) > (
           Time.now - (7*86400)
         )
